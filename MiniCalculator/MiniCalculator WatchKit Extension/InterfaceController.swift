90af6111577106db3f23c83f079d650d69d4ce53
gpessios <gpessios@gmail.com>
2015-12-01 15:16:23 +0200

    MiniCalculator app first commit with layout done.

---
+++ MiniCalculator/MiniCalculator WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MiniCalculator WatchKit Extension
+//
+//  Created by Georgios Pessios on 01/12/15.
+//  Copyright © 2015 Georgios Pessios. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
